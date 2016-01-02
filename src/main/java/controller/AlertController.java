59c524393279874135f893524f1def52c3786de2
Himanshu Pandey <himanshupandey.in@gmail.com>
2016-01-01 23:17:03 -0800

    EvoAlertServer Basic functionality done.

---
+++ src/main/java/controller/AlertController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlertController {
+	private static final Logger logger = LoggerFactory.getLogger(AlertController.class);
+
+    List<String> alertList = new ArrayList<String>() ;
+	
+    @RequestMapping(value = RestURIConstants.INGEST_ALERT, method = RequestMethod.POST)
+    public @ResponseBody String ingestAlert(@RequestBody String alertJsonString) {
+        logger.info("Start create alert.");
+        alertList.add(alertJsonString);
+        return alertJsonString;
+    }
+    
+    @RequestMapping(value = RestURIConstants.GET_LATEST_ALERT, method = RequestMethod.GET)
+    public @ResponseBody String getLatestAlert() {
+        logger.info("Start get latest alert");
+        if(alertList.size() > 0)
+        	return alertList.get(0).toString();
+        else
+        	return "No Alerts to show";
+    }
+    
+    @RequestMapping(value = RestURIConstants.GET_ALL_ALERTS, method = RequestMethod.GET)
+    public @ResponseBody String getAllAlerts() {
+        logger.info("Start get all alerts");
+        
+        if(alertList.size() > 0) {
+        	/* Return Array List of Json Objects */
+        	return alertList.toString();
+        } else {
+        	return "No Alerts to show";
+        }
+    }
+}
---
+++ src/main/java/controller/RestURIConstants.java
@@ -0,0 +1,15 @@
+package controller;
+ 
+public class RestURIConstants {
+ 
+    public static final String DUMMY_EMP = "/rest/emp/dummy";
+    public static final String GET_EMP = "/rest/emp/{id}";
+    public static final String GET_ALL_EMP = "/rest/emps";
+    public static final String CREATE_EMP = "/rest/emp/create";
+    public static final String DELETE_EMP = "/rest/emp/delete/{id}";
+     
+    public static final String INGEST_ALERT = "/rest/ingest/alert";
+    public static final String GET_LATEST_ALERT = "/rest/get/alert";
+    public static final String GET_ALL_ALERTS= "/rest/get/alerts";
+    
+}
